6d2ee312eb5982e55b1b9d016a3f4e84b3e7c3c3
rodrigotome <rodrigo.tome@habitant.es>
2022-07-20 17:58:21 +0200

    refactor: added script setup to app and character-cards

--- src/components/CharacterCards.vue
+++ src/components/CharacterCards.vue
@@ -1,3 +1,30 @@
+<script setup>
+import axios from "axios";
+import orderBy from "lodash/orderby";
+import { ref, computed } from 'vue';
+
+const characters = ref([]);
+const loadingState = ref(null);
+const orderKey = ref('id');
+const charactersOrdered = computed(() => orderBy(characters.value, orderKey.value));
+const setOrderKey = (key) => orderKey.value = key;
+const fetchAllCharacters = async  () => {
+  loadingState.value = "loading";
+
+  try {
+    const response = await axios.get("https://rickandmortyapi.com/api/character");
+    setTimeout(() => {
+      loadingState.value = "success";
+      characters.value = response.data.results;
+    }, 1000);
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+fetchAllCharacters();
+</script>
+
 <template>
   <div>
     <div class="border-b-2 pb-4 border-gray-300 text-center">
@@ -39,40 +66,6 @@
     </div>
   </div>
 </template>
-
-<script>
-import axios from "axios";
-import orderBy from "lodash/orderby";
-import { ref, computed } from 'vue';
-
-export default {
-  setup() {
-    const characters = ref([]);
-    const loadingState = ref(null);
-    const orderKey = ref('id');
-    const setOrderKey = (key) => orderKey.value = key;
-    const fetchAllCharacters = async  () => {
-      loadingState.value = "loading";
-
-      try {
-        const response = await axios.get("https://rickandmortyapi.com/api/character");
-        setTimeout(() => {
-          loadingState.value = "success";
-          characters.value = response.data.results;
-        }, 1000);
-      } catch (error) {
-        console.error(error)
-      }
-    };
-    const charactersOrdered = computed(() => orderBy(characters.value, orderKey.value));
-
-    return { setOrderKey, fetchAllCharacters, loadingState, charactersOrdered };
-  },
-  created() {
-    this.fetchAllCharacters();
-  },
-}
-</script>
 
 <style scoped>
 .btn {
